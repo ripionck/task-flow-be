2ac2e4aff2b062d3ddc36a82f2df38cbeb07cc2d
ripionck <ripion.chakmar@gmail.com>
2025-02-15 23:37:59 +0600

    fixed user admin configuration

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, UserSettings
+from .models import User
 
 
 class CustomUserAdmin(UserAdmin):
@@ -16,4 +16,3 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(UserSettings)

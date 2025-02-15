bf43528aeb835d819cd6487fd9a9420ca8190ce3
ripionck <ripion.chakmar@gmail.com>
2025-02-15 16:14:37 +0600

    added boards admin configuration

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Board, Column, TeamMember
 
-# Register your models here.
+
+class ColumnInline(admin.TabularInline):
+    model = Column
+
+
+class TeamMemberInline(admin.TabularInline):
+    model = TeamMember
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'created_at')
+    search_fields = ('title', 'description')
+    inlines = [ColumnInline, TeamMemberInline]
+
+
+admin.site.register(Column)
+admin.site.register(TeamMember)

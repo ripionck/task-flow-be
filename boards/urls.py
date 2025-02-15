25f3975a36298b5bbe276ace82279b641d1e12e2
ripionck <ripion.chakmar@gmail.com>
2025-02-16 00:39:56 +0600

    fixed boards urls

--- boards/urls.py
+++ boards/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('boards/', views.BoardListView.as_view(), name='board-list'),
-    path('boards/<uuid:pk>/', views.BoardDetailView.as_view(), name='board-detail'),
+    path('', views.BoardListView.as_view(), name='board-list'),
+    path('<uuid:pk>/', views.BoardDetailView.as_view(), name='board-detail'),
 ]

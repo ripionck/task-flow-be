f86667856b0df90ae9948a84f9201969b4ebfd0a
ripionck <ripion.chakmar@gmail.com>
2025-02-15 16:18:45 +0600

    added boards permissions

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Board, Column, TeamMember
+from .serializers import BoardSerializer, ColumnSerializer, TeamMemberSerializer
+from .permissions import IsBoardOwnerOrAdmin
 
-# Create your views here.
+
+class BoardViewSet(viewsets.ModelViewSet):
+    serializer_class = BoardSerializer
+    permission_classes = [permissions.IsAuthenticated, IsBoardOwnerOrAdmin]
+
+    def get_queryset(self):
+        return Board.objects.filter(created_by=self.request.user)
+
+    def perform_create(self, serializer):
+        board = serializer.save(created_by=self.request.user)
+        TeamMember.objects.create(
+            user=self.request.user,
+            board=board,
+            role='owner'
+        )
+
+
+class ColumnViewSet(viewsets.ModelViewSet):
+    serializer_class = ColumnSerializer
+    permission_classes = [permissions.IsAuthenticated, IsBoardOwnerOrAdmin]
+
+    def get_queryset(self):
+        return Column.objects.filter(board_id=self.kwargs['board_pk'])
+
+    def perform_create(self, serializer):
+        serializer.save(board_id=self.kwargs['board_pk'])
+
+
+class TeamMemberViewSet(viewsets.ModelViewSet):
+    serializer_class = TeamMemberSerializer
+    permission_classes = [permissions.IsAuthenticated, IsBoardOwnerOrAdmin]
+
+    def get_queryset(self):
+        return TeamMember.objects.filter(board_id=self.kwargs['board_pk'])
+
+    def perform_create(self, serializer):
+        serializer.save(board_id=self.kwargs['board_pk'])

a1b89642a28d653809f48fd1f1a529f92f34f7b9
ripionck <ripion.chakmar@gmail.com>
2025-02-15 16:26:27 +0600

    added tasks urls, views and serializers

---
+++ tasks/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TaskViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='task')
+
+urlpatterns = [
+    path('boards/<uuid:board_pk>/', include(router.urls)),
+    path('tasks/<uuid:task_pk>/comments/',
+         CommentViewSet.as_view({'get': 'list', 'post': 'create'}),
+         name='task-comments'),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Task, Comment
+from .serializers import TaskSerializer, CommentSerializer
+from boards.models import Board
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(board_id=self.kwargs['board_pk'])
+
+    def perform_create(self, serializer):
+        board = Board.objects.get(pk=self.kwargs['board_pk'])
+        serializer.save(
+            board=board,
+            column=board.columns.first(),
+            created_by=self.request.user
+        )
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['board'] = Board.objects.get(pk=self.kwargs['board_pk'])
+        return context
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Comment.objects.filter(task_id=self.kwargs['task_pk'])
+
+    def perform_create(self, serializer):
+        serializer.save(
+            task_id=self.kwargs['task_pk'],
+            user=self.request.user
+        )

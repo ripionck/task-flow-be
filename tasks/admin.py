4b85b7f78575ee4e5c3cd184a440a073113930c2
ripionck <ripion.chakmar@gmail.com>
2025-02-15 16:24:07 +0600

    added tasks admin configuration

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Task, Comment
 
-# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'board', 'status', 'priority')
+    list_filter = ('status', 'priority')
+    inlines = [CommentInline]
+
+
+admin.site.register(Comment)
